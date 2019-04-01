3d573a7de93cfd5cb0e1a364d74ac3c8760a56c4
Austin C <acouey119@gmail.com>
2019-04-01 17:24:18 -0400

    Added search functionality

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,21 +14,21 @@ export default Controller.extend({
       //alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
       //this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
       //this.set('emailAddress', '');
-      var game = this.game;
-      var genre = this.genre;
-      var company = this.company;
-      var released = this.released;
-      var platform = this.platform;
+      const game = this.game;
+      const genre = this.genre;
+      const company = this.company;
+      const released = this.released;
+      const platform = this.platform;
 
-      var newInvitation = this.store.createRecord('game', { name: game,  company: company, genre: genre, released: released, platform: platform});
-      newInvitation.save();
-
-      this.set('responseMessage', `Thank you! We have just saved your game: ${this.get('game')}`);
-      this.set('game', '');
-      this.set('genre', '');
-      this.set('company', '');
-      this.set('platform', '');
-      this.set('released', '');
+      const newInvitation = this.store.createRecord('game', { name: game,  company: company, genre: genre, released: released, platform: platform});
+      newInvitation.save().then(response => {
+        this.set('responseMessage', `Thank you! We have just saved your game: ${this.get('game')}`);
+        this.set('game', '');
+        this.set('genre', '');
+        this.set('company', '');
+        this.set('platform', '');
+        this.set('released', '');
+      });
     }
   }
 
--- app/routes/games.js
+++ app/routes/games.js
@@ -1,7 +1,23 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	model() {
-    return this.store.findAll('game');
-  }
+
+	queryParams:{
+		limit: {refreshModel: true},
+		letter: {refreshModel: true}
+	},
+
+	model(params) {
+		if(params.limit === 'all'){
+    		return this.store.findAll('game');
+		}
+
+		return this.store.query('game', {
+			
+	      orderBy: 'name',
+	      startAt: params.letter,
+	      endAt: params.letter+"\uf8ff"
+	    });
+  	}
+  
 });
